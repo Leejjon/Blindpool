a498391af5327538a56e6ecf7adcb0dacafc01e8
Leejjon <leejjon@gmail.com>
2018-09-19 00:17:31 +0200

    Added jax-rs to the project to make a pretty restful API.

---
+++ backend/src/main/java/net/leejjon/blindpool/api/PoolApi.java
@@ -0,0 +1,22 @@
+package net.leejjon.blindpool.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("v1/pool")
+public class PoolApi {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
