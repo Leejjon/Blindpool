f5e678cb8c03aca61624f24ef39846ccf1abc472
Leejjon <leejjon@gmail.com>
2018-09-22 01:10:14 +0200

    Implemented the PoolApi.

--- backend/src/main/java/net/leejjon/blindpool/api/PoolApi.java
+++ backend/src/main/java/net/leejjon/blindpool/api/PoolApi.java
@@ -1,22 +1,34 @@
 package net.leejjon.blindpool.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import net.leejjon.blindpool.model.Pool;
+import net.leejjon.blindpool.storage.PoolDataStore;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("v1/pool")
 public class PoolApi {
+    private final static Logger log = Logger.getLogger(PoolApi.class.getName());
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Path("{poolId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPool(@PathParam("poolId") String poolId) {
+        try {
+            if (poolId != null && !poolId.isEmpty()) {
+                Pool pool = PoolDataStore.getInstance().getPool(poolId).orElseThrow(NotFoundException::new);
+                return Response.ok(pool).build();
+            } else {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        } catch (NotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
