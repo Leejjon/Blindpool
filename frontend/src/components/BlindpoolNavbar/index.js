db9a9c06bbfebf64d9b73cbf768603e8980ee3aa
Leejjon <leejjon@gmail.com>
2018-10-21 00:15:33 +0200

    Fixed search engine problem with robots.txt

--- frontend/src/components/BlindpoolNavbar/index.js
+++ frontend/src/components/BlindpoolNavbar/index.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
+import Link from 'react-router-dom';
 
 class BlindPoolNavbar extends Component {
     render() {
         return (
             <AppBar color="primary">
-                <Typography align="right" variant="h6" color="inherit">
-                    Language&nbsp;
+
+                <Typography align="right" variant="body1" color="inherit">
+                    <a href="/blabla.html">Language</a>&nbsp;
                 </Typography>
             </AppBar>
         );
