46e7d91f394327ddbc3b6ddbec6a38d562764dbf
Leejjon <leejjon@gmail.com>
2018-11-09 04:54:50 +0100

    Added dropdown icon.

--- frontend/src/components/BlindpoolNavbar/index.js
+++ frontend/src/components/BlindpoolNavbar/index.js
@@ -4,6 +4,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
 import Link from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
 class BlindPoolNavbar extends Component {
     render() {
@@ -11,6 +12,7 @@ class BlindPoolNavbar extends Component {
             <AppBar color="primary">
                 <Button>
                     <Typography variant="subtitle2">Language</Typography>
+                    <Icon color="secondary">arrow_drop_down</Icon>
                 </Button>
             </AppBar>
         );
