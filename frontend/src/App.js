bff20e704ecde26b62b2e44be48929abb93a5326
Leejjon <leejjon@gmail.com>
2018-11-09 04:46:21 +0100

    Figured out a bit how to style the components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,12 +16,25 @@ const theme = createMuiTheme({
         },
         secondary: {
             light: '#0066ff',
-            main: '#0044ff',
+            main: '#ffffff',
             // dark: will be calculated from palette.secondary.main,
             contrastText: '#ffcc00',
         },
         // error: will use the default color
     },
+    typography: {
+        fontFamily: "\"Roboto\", \"Helvetica\", \"Arial\", sans-serif",
+        fontSize: 14,
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        subtitle2: {
+            color: "#ffffff",
+        },
+        button: {
+            textTransform: 'none'
+        }
+    }
 });
 
 class App extends Component {
--- frontend/src/components/BlindpoolNavbar/index.js
+++ frontend/src/components/BlindpoolNavbar/index.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
 import Link from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 class BlindPoolNavbar extends Component {
     render() {
         return (
             <AppBar color="primary">
-
-                <Typography align="right" variant="body1" color="inherit">
-                    <a href="/blabla.html">Language</a>&nbsp;
-                </Typography>
+                <Button>
+                    <Typography variant="subtitle2">Language</Typography>
+                </Button>
             </AppBar>
         );
     }
