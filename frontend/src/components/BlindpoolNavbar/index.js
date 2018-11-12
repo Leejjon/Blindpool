ffe08a126dd194fc18c27f390ea171553046f675
Leejjon <leejjon@gmail.com>
2018-11-13 00:28:25 +0100

    Managed to align the language to be dropdown box to the right side.

--- frontend/src/components/BlindpoolNavbar/index.js
+++ frontend/src/components/BlindpoolNavbar/index.js
@@ -5,15 +5,24 @@ import Typography from '@material-ui/core/Typography';
 import Link from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
+import Grid from "@material-ui/core/Grid/Grid";
 
 class BlindPoolNavbar extends Component {
     render() {
         return (
             <AppBar color="primary">
-                <Button>
-                    <Typography variant="subtitle2">Language</Typography>
-                    <Icon color="secondary">arrow_drop_down</Icon>
-                </Button>
+                {/* I suppose you put stuff in grids and then give grids positions instead of positioning separate buttons. Makes sense I guess. */}
+                <Grid
+                    justify="flex-end"
+                    container
+                    spacing={24}>
+                    <Grid item>
+                        <Button>
+                            <Typography variant="subtitle2">Language</Typography>
+                            <Icon color="secondary">arrow_drop_down</Icon>
+                        </Button>
+                    </Grid>
+                </Grid>
             </AppBar>
         );
     }
