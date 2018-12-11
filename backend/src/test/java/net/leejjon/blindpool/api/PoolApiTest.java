e42c685a8f6d2730cd151ae17009aa52b128d368
Leejjon <leejjon@gmail.com>
2018-12-11 11:06:57 +0100

    Added sample test to demonstrate the bug.

--- backend/src/test/java/net/leejjon/blindpool/api/PoolApiTest.java
+++ backend/src/test/java/net/leejjon/blindpool/api/PoolApiTest.java
@@ -17,59 +17,58 @@ import javax.ws.rs.core.Response;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PoolApiTest {
 
-    private PoolApi poolApi;
-
-    @Mock
-    private PoolDataServiceImpl poolDataService;
-
-    @Test
-    public void testCreatePool_success() {
-        Pool poolToReturn = getPoolFourParticipants();
-        when(poolDataService.createPool(any())).thenReturn(poolToReturn);
-
-        poolApi = new PoolApi(poolDataService);
-        Response poolResponse = poolApi.createPool(Arrays.asList("Leon", "Dirk", "Robert", "Jaimy"));
-        assertEquals(200, poolResponse.getStatus());
-
-        String jsonString = new Gson().toJson(poolResponse.getEntity());
-        Pool poolFromJsonString = new Gson().fromJson(jsonString, Pool.class);
-        assertEquals(poolToReturn, poolFromJsonString);
-    }
-
-    @Test
-    public void testGetPool_success() throws EntityNotFoundException {
-        Pool poolToReturn = getPoolFourParticipants();
-        when(poolDataService.getPool(any())).thenReturn(poolToReturn);
-
-        poolApi = new PoolApi(poolDataService);
-        Response poolResponse = poolApi.getPool("anyPool");
-        assertEquals(200, poolResponse.getStatus());
-    }
-
-    private Pool getPoolFourParticipants() {
-        Participant participant1 = new Participant("Leon");
-        Participant participant2 = new Participant("Dirk");
-        Participant participant3 = new Participant("Robert");
-        Participant participant4 = new Participant("Jaimy");
-
-        Score score1 = new Score(ScoreGenerator.WILDCARD_CHARACTER, ScoreGenerator.WILDCARD_CHARACTER);
-        Score score2 = new Score('0', '0');
-        Score score3 = new Score('1', '0');
-        Score score4 = new Score('0', '1');
-
-        List<ParticipantScore> participantScoreList = Arrays.asList(
-                new ParticipantScore(participant1, score1),
-                new ParticipantScore(participant2, score2),
-                new ParticipantScore(participant3, score3),
-                new ParticipantScore(participant4, score4));
-
-        return Pool.builder().key("1").participantsAndScores(participantScoreList).createdTimestamp(System.currentTimeMillis()).build();
-    }
+//    private PoolApi poolApi;
+//
+//    @Mock
+//    private PoolDataServiceImpl poolDataService;
+//
+//    @Test
+//    public void testCreatePool_success() {
+//        Pool poolToReturn = getPoolFourParticipants();
+//        when(poolDataService.createPool(any())).thenReturn(poolToReturn);
+//
+//        poolApi = new PoolApi(poolDataService);
+//        Response poolResponse = poolApi.createPool(Arrays.asList("Leon", "Dirk", "Robert", "Jaimy"));
+//        assertEquals(200, poolResponse.getStatus());
+//
+//        String jsonString = new Gson().toJson(poolResponse.getEntity());
+//        Pool poolFromJsonString = new Gson().fromJson(jsonString, Pool.class);
+//        assertEquals(poolToReturn, poolFromJsonString);
+//    }
+//
+//    @Test
+//    public void testGetPool_success() throws EntityNotFoundException {
+//        Pool poolToReturn = getPoolFourParticipants();
+//        when(poolDataService.getPool(any())).thenReturn(poolToReturn);
+//
+//        poolApi = new PoolApi(poolDataService);
+//        Response poolResponse = poolApi.getPool("anyPool");
+//        assertEquals(200, poolResponse.getStatus());
+//    }
+//
+//    private Pool getPoolFourParticipants() {
+//        Participant participant1 = new Participant("Leon");
+//        Participant participant2 = new Participant("Dirk");
+//        Participant participant3 = new Participant("Robert");
+//        Participant participant4 = new Participant("Jaimy");
+//
+//        Score score1 = new Score(ScoreGenerator.WILDCARD_CHARACTER, ScoreGenerator.WILDCARD_CHARACTER);
+//        Score score2 = new Score('0', '0');
+//        Score score3 = new Score('1', '0');
+//        Score score4 = new Score('0', '1');
+//
+//        List<ParticipantScore> participantScoreList = Arrays.asList(
+//                new ParticipantScore(participant1, score1),
+//                new ParticipantScore(participant2, score2),
+//                new ParticipantScore(participant3, score3),
+//                new ParticipantScore(participant4, score4));
+//
+//        return Pool.builder().key("1").participantsAndScores(participantScoreList).createdTimestamp(System.currentTimeMillis()).build();
+//    }
 }
