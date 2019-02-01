b91e440384c7b89a7fad033041fc7891c293686b
Leejjon <leejjon@gmail.com>
2019-02-01 01:31:50 +0100

    T:/

--- frontend/src/components/BlindpoolNavbar/index.js
+++ frontend/src/components/BlindpoolNavbar/index.js
@@ -7,6 +7,24 @@ import Icon from '@material-ui/core/Icon';
 import Grid from "@material-ui/core/Grid/Grid";
 import Menu from "@material-ui/core/Menu/Menu";
 import FlagIcon from "../../locales/FlagIcon";
+import Toolbar from "@material-ui/core/Toolbar";
+import * as PropTypes from "prop-types";
+import {withStyles} from '@material-ui/core/styles';
+import intl from 'react-intl-universal';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+
+        marginLeft: 12,
+        marginRight: 20,
+    },
+};
 
 class BlindPoolNavbar extends Component {
     // I have no fucking clue what the anchorEI is doing. But it probably has something to do with the menu.
@@ -17,11 +35,11 @@ class BlindPoolNavbar extends Component {
     };
 
     openLanguageSelect = event => {
-        this.setState({ anchorEl: event.currentTarget });
+        this.setState({anchorEl: event.currentTarget});
     };
 
     closeLanguageSelect = () => {
-        this.setState({ anchorEl: null });
+        this.setState({anchorEl: null});
         console.log("Clicked outside of the menu and thus closed language select.");
     };
 
@@ -34,51 +52,71 @@ class BlindPoolNavbar extends Component {
             lastTwoCharacters = 'gb';
         }
 
-        this.setState({ anchorEl: null, currentLang: lastTwoCharacters});
+        this.setState({anchorEl: null, currentLang: lastTwoCharacters});
         console.log(lastTwoCharacters);
         this.props.setLanguage(lang);
     };
 
     currentFlag() {
         if (this.state.currentLang !== null) {
-            return <FlagIcon code={this.state.currentLang} size="lg" />
+            return <FlagIcon code={this.state.currentLang} size="lg"/>
         } else {
             return <FlagIcon code="gb" size="lg"/>
         }
     }
 
     render() {
-        const { anchorEl } = this.state;
+        const {anchorEl} = this.state;
         return (
-            <AppBar color="primary">
-                {/* I suppose you put stuff in grids and then give grids positions instead of positioning separate buttons. Makes sense I guess. */}
-                <Grid
-                    justify="flex-end"
-                    container
-                    spacing={24}>
-                    <Grid item>
-                        {/*<Typography>{intl.get('TITLE')}</Typography>*/}
-                    </Grid>
-                    <Grid item>
-                        <Button aria-owns={anchorEl ? 'simple-menu' : undefined}
-                                aria-haspopup="true"
+            {/* https://material-ui.com/demos/app-bar/ */}
+            <div>
+                <AppBar color="primary" position="static">
+                    <Toolbar>
+                        <Typography variant="body1" color="inherit">
+                            {intl.get('TITLE')}
+                        </Typography>
+                        <Button aria-owns={anchorEl ? 'simple-menu' : undefined} aria-haspopup="true"
                                 onClick={this.openLanguageSelect}>
                             {this.currentFlag()}
                             <Icon color="secondary">arrow_drop_down</Icon>
                         </Button>
-                        <Menu
-                            id="simple-menu"
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={this.closeLanguageSelect}>
-                            <MenuItem onClick={this.selectLanguage.bind(this, 'en-US')}><FlagIcon code="gb" size="lg"/>&nbsp;&nbsp;English</MenuItem>
-                            <MenuItem onClick={this.selectLanguage.bind(this, 'nl-NL')}><FlagIcon code="nl" size="lg"/>&nbsp;&nbsp;Nederlands</MenuItem>
+                        <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)}
+                              onClose={this.closeLanguageSelect}>
+                            <MenuItem onClick={this.selectLanguage.bind(this, 'en-US')}><FlagIcon code="gb"
+                                                                                                  size="lg"/>&nbsp;&nbsp;English</MenuItem>
+                            <MenuItem onClick={this.selectLanguage.bind(this, 'nl-NL')}><FlagIcon code="nl"
+                                                                                                  size="lg"/>&nbsp;&nbsp;Nederlands</MenuItem>
                         </Menu>
-                    </Grid>
-                </Grid>
-            </AppBar>
+                    </Toolbar>
+                    {/* I suppose you put stuff in grids and then give grids positions instead of positioning separate buttons. Makes sense I guess. */}
+                    {/*<Grid*/}
+                    {/*justify="flex-end"*/}
+                    {/*container*/}
+                    {/*spacing={24}>*/}
+                    {/*<Grid item>*/}
+                    {/*/!*<Typography>{intl.get('TITLE')}</Typography>*!/*/}
+                    {/*</Grid>*/}
+                    {/*<Grid item>*/}
+                    {/*<Button aria-owns={anchorEl ? 'simple-menu' : undefined}*/}
+                    {/*aria-haspopup="true"*/}
+                    {/*onClick={this.openLanguageSelect}>*/}
+                    {/*{this.currentFlag()}*/}
+                    {/*<Icon color="secondary">arrow_drop_down</Icon>*/}
+                    {/*</Button>*/}
+                    {/*<Menu*/}
+                    {/*id="simple-menu"*/}
+                    {/*anchorEl={anchorEl}*/}
+                    {/*open={Boolean(anchorEl)}*/}
+                    {/*onClose={this.closeLanguageSelect}>*/}
+                    {/*<MenuItem onClick={this.selectLanguage.bind(this, 'en-US')}><FlagIcon code="gb" size="lg"/>&nbsp;&nbsp;English</MenuItem>*/}
+                    {/*<MenuItem onClick={this.selectLanguage.bind(this, 'nl-NL')}><FlagIcon code="nl" size="lg"/>&nbsp;&nbsp;Nederlands</MenuItem>*/}
+                    {/*</Menu>*/}
+                    {/*</Grid>*/}
+                    {/*</Grid>*/}
+                </AppBar>
+            </div>
         );
     }
 }
 
-export default BlindPoolNavbar;
+export default withStyles(styles)(BlindPoolNavbar);
