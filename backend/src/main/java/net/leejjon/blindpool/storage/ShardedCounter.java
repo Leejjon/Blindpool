2b60eaa32057ae13079f49b225e69e2289cd8a5b
Leejjon <leejjon@gmail.com>
2019-03-15 01:11:36 +0100

    Work in progress with the sharded counter.

--- backend/src/main/java/net/leejjon/blindpool/storage/ShardedCounter.java
+++ backend/src/main/java/net/leejjon/blindpool/storage/ShardedCounter.java
@@ -1,13 +1,17 @@
 package net.leejjon.blindpool.storage;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Transaction;
+//import com.google.appengine.api.datastore.DatastoreService;
+//import com.google.appengine.api.datastore.DatastoreServiceFactory;
+//import com.google.appengine.api.datastore.Entity;
+//import com.google.appengine.api.datastore.EntityNotFoundException;
+//import com.google.appengine.api.datastore.Key;
+//import com.google.appengine.api.datastore.KeyFactory;
+//import com.google.appengine.api.datastore.Query;
+//import com.google.appengine.api.datastore.Transaction;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
 import net.leejjon.blindpool.storage.persistence.KindType;
 
 import java.util.ConcurrentModificationException;
@@ -26,8 +30,11 @@ public class ShardedCounter {
     /**
      * DatastoreService object for Datastore access.
      */
-    private static final DatastoreService DS = DatastoreServiceFactory
-            .getDatastoreService();
+//    private static final DatastoreService DS = DatastoreServiceFactory
+//            .getDatastoreService();
+
+    private static final Datastore DS = DatastoreOptions.getDefaultInstance().getService();
+
 
     /**
      * Default number of shards.
@@ -53,10 +60,11 @@ public class ShardedCounter {
     public final long count() {
         long sum = 0;
 
-        Query query = new Query(KindType.POOL_COUNTER_SHARD.toString());
-        for (Entity e : DS.prepare(query).asIterable()) {
-            sum += (Long) e.getProperty("count");
-        }
+//        Query query = new Query(KindType.POOL_COUNTER_SHARD.toString());
+//        for (Entity e : DS.prepare(query).asIterable()) {
+//            sum += (Long) e.getProperty("count");
+//        }
+
 
         return sum;
     }
@@ -66,32 +74,50 @@ public class ShardedCounter {
      */
     public final void increment() {
         int shardNum = generator.nextInt(NUM_SHARDS);
-        Key shardKey = KeyFactory.createKey(KindType.POOL_COUNTER_SHARD.toString(),
-                Integer.toString(shardNum));
 
-        Transaction tx = DS.beginTransaction();
-        Entity shard;
-        try {
-            try {
-                shard = DS.get(tx, shardKey);
-                long count = (Long) shard.getProperty("count");
-                shard.setUnindexedProperty("count", count + 1L);
-            } catch (EntityNotFoundException e) {
-                shard = new Entity(shardKey);
-                shard.setUnindexedProperty("count", 1L);
-            }
-            DS.put(tx, shard);
-            tx.commit();
-        } catch (ConcurrentModificationException e) {
-            LOG.log(Level.WARNING,
-                    "You may need more shards. Consider adding more shards.");
-            LOG.log(Level.WARNING, e.toString(), e);
-        } catch (Exception e) {
-            LOG.log(Level.WARNING, e.toString(), e);
-        } finally {
-            if (tx.isActive()) {
-                tx.rollback();
-            }
-        }
+        Key shardKey = DS.newKeyFactory()
+                .setKind(KindType.POOL_COUNTER_SHARD.toString())
+                .newKey(Integer.toString(shardNum));
+
+//        Key shardKey = KeyFactory.createKey(KindType.POOL_COUNTER_SHARD.toString(),
+//                Integer.toString(shardNum));
+
+        Entity shard = DS.get(shardKey);
+
+        long count = shard.getLong("count");
+
+        Entity updatedShard = Entity.newBuilder(shardKey)
+        .set("count", "Personal")
+        .build();
+
+        // Saves the entity
+        DS.put(task);
+
+
+//
+//        Transaction tx = DS.beginTransaction();
+//        Entity shard;
+//        try {
+//            try {
+//                shard = DS.get(tx, shardKey);
+//                long count = (Long) shard.getProperty("count");
+//                shard.setUnindexedProperty("count", count + 1L);
+//            } catch (EntityNotFoundException e) {
+//                shard = new Entity(shardKey);
+//                shard.setUnindexedProperty("count", 1L);
+//            }
+//            DS.put(tx, shard);
+//            tx.commit();
+//        } catch (ConcurrentModificationException e) {
+//            LOG.log(Level.WARNING,
+//                    "You may need more shards. Consider adding more shards.");
+//            LOG.log(Level.WARNING, e.toString(), e);
+//        } catch (Exception e) {
+//            LOG.log(Level.WARNING, e.toString(), e);
+//        } finally {
+//            if (tx.isActive()) {
+//                tx.rollback();
+//            }
+//        }
     }
 }
