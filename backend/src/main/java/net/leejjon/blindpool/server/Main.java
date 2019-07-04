a47ff3732e792f0add3b13ce10c1037332c913d6
Leejjon <leejjon@gmail.com>
2019-07-04 23:49:05 +0200

    Inverted the if statements, classic mistake.

--- backend/src/main/java/net/leejjon/blindpool/server/Main.java
+++ backend/src/main/java/net/leejjon/blindpool/server/Main.java
@@ -1,27 +1,26 @@
 package net.leejjon.blindpool.server;
 
 import com.google.cloud.datastore.DatastoreOptions;
-import net.leejjon.blindpool.api.HealthApi;
-import net.leejjon.blindpool.storage.PoolDataServiceImpl;
-import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import net.leejjon.blindpool.api.HealthApi;
 import net.leejjon.blindpool.api.PoolApi;
+import net.leejjon.blindpool.storage.PoolDataServiceImpl;
 
 import java.net.URI;
 
 public class Main {
-    static final String PORT = System.getenv().get("PORT");
+    private static final String DEFAULT_PORT = System.getenv().get("PORT");
+    private static final String DEFAULT_HOST = "0.0.0.0";
 
     // Base URI the Grizzly HTTP server will listen on
-    static final String BASE_URI = "http://%1$s:%2$s/";
+    private static final String BASE_URI = "http://%1$s:%2$s/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
      */
-    static HttpServer startServer(String base) {
+    private static void startServer(String base, String port) {
         // create a resource config that registers the MyResource JAX-RS resource
         final ResourceConfig rc = new ResourceConfig();
 
@@ -38,7 +37,7 @@ public class Main {
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(String.format(BASE_URI, base, PORT)), rc);
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(String.format(BASE_URI, base, DEFAULT_PORT)), rc);
     }
 
     /**
@@ -46,8 +45,9 @@ public class Main {
      * @param args Should contain "localhost" or "0.0.0.0".
      */
     public static void main(String[] args) {
-        String base = args.length == 0 ? "0.0.0.0" : args[0];
-        startServer(base);
-        System.out.println(String.format("Jersey app started at %s", String.format(BASE_URI, base, PORT)));
+        String base = args.length > 0 ? args[0] : DEFAULT_HOST;
+        String port = args.length > 1 ? args[1] : DEFAULT_PORT;
+        startServer(base, port);
+        System.out.println(String.format("Jersey app started at %s", String.format(BASE_URI, base, port)));
     }
 }
