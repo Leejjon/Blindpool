e9206585702b393d3bb22a80df0c152307fb75bc
Leejjon <leejjon@gmail.com>
2020-02-01 12:47:23 +0100

    Made the locale and hostname logic more readable.

--- frontend/src/components/bphelmet/BpDefaultHelmet.tsx
+++ frontend/src/components/bphelmet/BpDefaultHelmet.tsx
@@ -4,12 +4,8 @@ import Helmet from "react-helmet";
 import {RouteComponentProps} from "react-router";
 
 const BpDefaultHelmet: React.FC<RouteComponentProps<any>> = (props) => {
-
-    // TODO: Simply put the url in the translation
-    const locale = window.location.hostname === "www.blindepool.nl" ||
-    window.location.hostname === "blindepool.nl" ? 'en' : 'nl';
-    const hostname = window.location.hostname === "www.blindepool.nl" ||
-    window.location.hostname === "blindepool.nl" ? 'http://blindpool.com' : 'https://blindepool.nl';
+    const locale = window.location.hostname.endsWith("blindepool.nl") ? 'en' : 'nl';
+    const hostname = window.location.hostname.endsWith("blindepool.nl") ? 'https://blindpool.com' : 'https://blindepool.nl';
 
     return (
         <Helmet>
