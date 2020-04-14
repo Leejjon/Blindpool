232a18cfeed82f0da6a0ee8dacb8551592325721
Leejjon <leejjon@gmail.com>
2020-04-15 00:20:54 +0200

    Managed to get the simulator running.

---
+++ blindpool-api/set_vars.cmd
@@ -0,0 +1,5 @@
+set DATASTORE_DATASET=nodejs-static
+set DATASTORE_EMULATOR_HOST=localhost:8081
+set DATASTORE_EMULATOR_HOST_PATH=localhost:8081/datastore
+set DATASTORE_HOST=http://localhost:8081
+set DATASTORE_PROJECT_ID=nodejs-static
--- blindpool-api/src/services/BlindpoolStorageService.ts
+++ blindpool-api/src/services/BlindpoolStorageService.ts
@@ -1,7 +1,9 @@
-/**
- * Data Model Interfaces
- */
+import { Datastore } from '@google-cloud/datastore';
 import { Blindpool } from '../models/Blindpool'
+import {RunQueryResponse} from "@google-cloud/datastore/build/src/query";
+
+// Instantiate a datastore client
+const datastore = new Datastore();
 
 const pool: Blindpool = {
     key: '123',
@@ -11,8 +13,21 @@ const pool: Blindpool = {
 
 export const find = async (key: string): Promise<Blindpool> => {
     console.log(`Requesting blindpool with key: ${key}`);
+
+    const [entities]: RunQueryResponse = await getPools();
+
+    console.log(`${entities.length}`);
+
     if (key === pool.key) {
         return pool;
     }
     throw new Error("No record found");
+};
+
+const getPools = () => {
+    const query = datastore
+        .createQuery('pool')
+        .order('CREATED_TIMESTAMP', {descending: true})
+        .limit(10);
+    return datastore.runQuery(query);
 };
