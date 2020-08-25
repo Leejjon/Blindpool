b104d12cf661ca91a3bfc7fc8f88d5d9fa3b1b08
Leejjon <leejjon@gmail.com>
2020-08-25 22:39:51 +0200

    Sorry for breaking.

--- matches-api/src/api/MatchesApi.ts
+++ matches-api/src/api/MatchesApi.ts
@@ -1,7 +1,7 @@
 import { Datastore } from "@google-cloud/datastore";
 import { Match } from "../model/Match";
 import { Request, Response } from "express";
-import { API_FOOTBAL_DATA_URL, EREDIVISIE } from "../constants";
+import { API_FOOTBAL_DATA_URL, EREDIVISIE_NAME, EREDIVISIE_CODE } from "../constants";
 import axios from "axios";
 import {fetchSecret} from "../services/SecretService";
 
@@ -13,56 +13,58 @@ export const getScheduledMatches = async (req: Request, res: Response) => {
 
     try {
         const matchesResponse = await axios.get(
-            `${API_FOOTBAL_DATA_URL}/competitions/${EREDIVISIE}/matches/`,
+            `${API_FOOTBAL_DATA_URL}/competitions/${EREDIVISIE_CODE}/matches/`,
             { headers: { "X-Auth-Token": await fetchSecret()} }
         );
+        
+        // console.log(`${JSON.stringify(matchesResponse.data)}`);
 
         matchesResponse?.data?.matches.map(async (match: any) => {
-            const result = {
-                date: match.utcDate,
-                competitionName: match.competition.name,
-                homeTeam: {
-                    name: match.homeTeam.name,
-                    score: match.score.fullTime.homeTeam
-                },
-                awayTeam: {
-                    name: match.awayTeam.name,
-                    score: match.score.fullTime.awayTeam
+
+            try {
+                const result = {
+                    date: match.utcDate,
+                    competitionName: match.competitionName,
+                    homeTeam: {
+                        name: match.homeTeam.name,
+                        score: match.score.fullTime.homeTeam
+                    },
+                    awayTeam: {
+                        name: match.awayTeam.name,
+                        score: match.score.fullTime.awayTeam
+                    }
+                } as Match;
+                const sourceId = datastore.key([matchSource, `football-data-${match.id}`]);
+                const resultKey = await datastore.get(sourceId);
+
+                if (resultKey?.length > 0 && resultKey[0]) {
+                    console.log("Found the key", resultKey)
+                    // const matchData = {
+                    //     key: storedResult.key,
+                    //     data: storedResult.data,
+                    // }
+                    // await datastore.update(matchData);
+                } else {
+                    console.log("Not found");
+                    // The Cloud Datastore key for the new entity
+                    const matchKey = datastore.key(matchKind);
+                    const matchData = {
+                        key: matchKey,
+                        data: result,
+                    };
+                    await datastore.save(matchData);
+                    console.log("saved match", matchData);
+                    const sourceIdData = {
+                        key: sourceId,
+                        data: { id: matchKey.id },
+                    }
+                    console.log("sourceIdData", sourceIdData);
+
+                    await datastore.save(sourceIdData);
                 }
-            } as Match;
-
-            console.log(`${JSON.stringify(result)}`);
-
-            const sourceId = datastore.key([matchSource, `football-data-${match.id}`]);
-            const resultKey = await datastore.get(sourceId);
-
-            if (resultKey?.length > 0 && resultKey[0]) {
-                console.log("Found the key", resultKey)
-                // const matchData = {
-                //     key: storedResult.key,
-                //     data: storedResult.data,
-                // }
-                // await datastore.update(matchData);
-            } else {
-                console.log("Not found");
-                // The Cloud Datastore key for the new entity
-                const matchKey = datastore.key(matchKind);
-                const matchData = {
-                    key: matchKey,
-                    data: result,
-                };
-                await datastore.save(matchData);
-                console.log("saved match", matchData);
-                const sourceIdData = {
-                    key: sourceId,
-                    data: { id: matchKey.id },
-                }
-                console.log("sourceIdData", sourceIdData);
-
-                await datastore.save(sourceIdData);
-
+            } catch (error) { // Not catching it will result in unresolved errors
+                console.log(error);
             }
-
         });
 
         res.contentType('application/json');
--- matches-api/src/constants.ts
+++ matches-api/src/constants.ts
@@ -1,2 +1,3 @@
 export const API_FOOTBAL_DATA_URL = "http://api.football-data.org/v2";
-export const EREDIVISIE = 2003;
+export const EREDIVISIE_CODE = 2003;
+export const EREDIVISIE_NAME = "Eredivisie";
