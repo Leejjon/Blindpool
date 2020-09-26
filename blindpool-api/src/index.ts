73aca23482d05c88e521c80a53eae2422c6c2692
Leejjon <leejjon@gmail.com>
2020-09-26 22:09:34 +0200

    Improved handling of invalid json syntax. No more stacktraces are logged but the incorrect request instead.

--- blindpool-api/src/index.ts
+++ blindpool-api/src/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, {NextFunction, Request, Response} from "express";
 import {getBlindpoolByKey, getBlindpoolStatistics, postCreateBlindpool} from "./api/BlindpoolApi";
 import cors from "cors";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || '8080';
 const environment = process.env.NODE_ENV || 'development';
 const router = express.Router();
 
@@ -19,9 +19,27 @@ router.get('/pool/:key', getBlindpoolByKey);
 const app = express();
 
 app.use(express.json());
+
+interface SyntaxErrorWithStatusAndBody extends SyntaxError {
+    status: number;
+    body: string;
+}
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // This check makes sure this is a JSON parsing issue, but it might be
+    // coming from any middleware, not just body-parser:
+    if ((err as SyntaxErrorWithStatusAndBody).status === 400 && 'body' in err) {
+        const bodyWithInvalidJson = (err as SyntaxErrorWithStatusAndBody).body;
+        console.error(`Somebody tried to do a request with invalid json: ${bodyWithInvalidJson}`);
+        return res.sendStatus(400); // Bad request
+    }
+    // If it's another error, let the default handler handle it.
+    next();
+});
+
 app.use('/api/v2/', router);
 
-const server = app.listen(port, () => {
+export const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
