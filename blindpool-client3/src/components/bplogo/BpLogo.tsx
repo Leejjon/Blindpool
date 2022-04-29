0e9233d29fad5ff8cb87394485af2b3e67589c09
Leejjon <leejjon@gmail.com>
2022-04-29 15:19:41 +0200

    Apparently styled components don't work in my setup. Fuck it just using css instead.

--- blindpool-client3/src/components/bplogo/BpLogo.tsx
+++ blindpool-client3/src/components/bplogo/BpLogo.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import {styled} from "@mui/material";
+import './BpLogo.css';
 
 const BpLogo: React.FC = () => {
-    return <img alt="BLINDPOOL" src={"/icons/logosmall.png"}/>
+    return <img id="logo" alt="BLINDPOOL" src={"/icons/logosmall.png"}/>
 }
 
-const StyledBpLogo = styled(BpLogo)(({theme}) => ({
-    width: "150px", height: "24px", marginTop: "20.5em"
-}))
-
-export default function StyledBpLogoFn() {
-    return <StyledBpLogo />
-}
+export default BpLogo;
