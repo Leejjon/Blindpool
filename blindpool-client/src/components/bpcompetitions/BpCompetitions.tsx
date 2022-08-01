5918428728f48d607b357fe4da12f0d69fdb6779
Leejjon <leejjon@gmail.com>
2022-08-01 14:14:57 +0200

    Attempt at getting premier league icons in.

--- blindpool-client/src/components/bpcompetitions/BpCompetitions.tsx
+++ blindpool-client/src/components/bpcompetitions/BpCompetitions.tsx
@@ -1,6 +1,7 @@
 import {Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import {competitions, getCompetitionsList} from "../../constants/Competitions";
 import {defaultCompetitions} from "../../locales/i18n";
+import {getHostnameWithPortIfLocal} from "../../utils/Network";
 
 
 const BpUpcomingMatches = () => {
@@ -9,11 +10,15 @@ const BpUpcomingMatches = () => {
             {getCompetitionsList().map((key: number) => {
                 const competition = competitions[key];
                 const labelId = `competition-label-${key}`;
+                `${window.location.protocol}//${getHostnameWithPortIfLocal()}/clubicons/${}.svg`;
                 return (
                     <ListItem sx={{margin: "0", padding: "0"}} key={key}>
                         <ListItemButton>
                             <ListItemIcon>
                                 <Checkbox edge="start" defaultChecked={defaultCompetitions.includes(key)} disableRipple color="secondary" />
+                            </ListItemIcon>
+                            <ListItemIcon>
+
                             </ListItemIcon>
                             <ListItemText id={labelId}>
                                 {competition.name}
