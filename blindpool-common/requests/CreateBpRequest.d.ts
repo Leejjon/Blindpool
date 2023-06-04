15f6a9a4b45e6ba3ba549ce7ba5d5e03338b3aaf
Leejjon <leejjon@gmail.com>
2023-06-04 23:50:31 +0200

    Accidently comitted some generated js files, and we want our project to be 100% typescript.

--- blindpool-common/requests/CreateBpRequest.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CreateBlindpoolRequest {
-    participants: string[];
-    selectedMatchID?: string;
-    freeFormatMatch?: string;
-    constructor(participants: string[], selectedMatch?: string, freeFormatMatch?: string);
-}
