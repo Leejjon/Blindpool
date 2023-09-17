0bf2428ee705f62474514e5ca7de5c43d7f0be39
Leejjon <leejjon@gmail.com>
2023-09-18 00:42:23 +0200

    I quickly added all German clubs.

--- blindpool-common/constants/Competitions.ts
+++ blindpool-common/constants/Competitions.ts
@@ -1,6 +1,7 @@
 
 
 export enum CompetitionEnum {
+    BUNDESLIGA = "Bundesliga",
     EREDIVISIE = "Eredivisie",
     PREMIER_LEAGUE = "Premier League",
     LA_LIGA = "La Liga",
@@ -13,6 +14,131 @@ interface Competition {
     competition: CompetitionEnum
     // In the future we maybe get picture information etc here.
     teams: {[key: number]: string}
+}
+
+const bundesLigaTeams: {[key: number]: string} = {
+    1: 'FC Köln',
+    2: 'Hoffenheim',
+    3: 'Leverkusen',
+    4: 'Dortmund',
+    5: 'Bayern',
+    10: 'VfL Stuttgart',
+    11: 'VfL Wolfsburg',
+    12: 'Werder Bremen',
+    15: 'FSV Mainz',
+    16: 'FC Augsburg',
+    17: 'SC Freiburg',
+    18: "M'gladbach",
+    19: "Frankfurt",
+    28: "Union Berlin",
+    36: 'VfL Bochum',
+    44: 'Heidenheim',
+    55: 'SV Darmstadt',
+    721: 'RB Leipzig'
+}
+
+const eredivisieTeams: {[key: number]: string} = {
+    666: 'FC Twente',
+    668: 'VVV Venlo',
+    671: 'Heracles',
+    672: 'Willem II',
+    673: 'SC Heerenveen',
+    674: 'PSV',
+    675: 'Feyenoord',
+    676: 'FC Utrecht',
+    677: 'FC Groningen',
+    678: 'Ajax',
+    679: 'Vitesse',
+    680: 'ADO Den Haag',
+    682: 'AZ',
+    683: 'RKC Waalwijk',
+    684: 'PEC Zwolle',
+    670: 'Excelsior',
+    718: 'Go Ahead Eagles',
+    1909: 'SC Cambuur',
+    1911: 'Almere City',
+    1914: 'FC Emmen',
+    1915: 'NEC Nijmegen',
+    1919: 'FC Volendam',
+    1920: 'Fortuna Sittard',
+    6806: 'Sparta'
+}
+
+const laLigaTeams: {[key: number]: string} = {
+    77: "Atletic Club",
+    78: "Atlético",
+    79: "CA Osasuna",
+    80: "Espanyol",
+    81: "FC Barcalona",
+    82: "Getafe CF",
+    83: "Granada",
+    86: "Real Madrid",
+    87: "Rayo Vallecano",
+    89: "RCD Mallorca",
+    90: "Real Betis",
+    92: "Real Sociedad",
+    94: "Villareal CF",
+    95: "Valencia CF",
+    250: "Valladolid",
+    263: "Alavés",
+    264: "Cádiz CF",
+    267: "UD Almería",
+    275: "Las Palmas",
+    285: "Elche CF",
+    298: "Girona FC",
+    558: "RC Celta de Vigo",
+    559: "Sevilla FC"
+}
+
+const serieATeams: {[key: number]: string} = {
+    98: "AC Milan",
+    99: "Fiorentina",
+    100: "AS Roma",
+    102: "Atalanta",
+    103: "Bologna",
+    104: "Cagliari",
+    107: "Genoa",
+    108: "Inter Milan",
+    109: "Juventus",
+    110: "Lazio",
+    113: "Napoli",
+    115: "Udinese",
+    445: "Empoli",
+    450: "Verona",
+    455: "Salernitana",
+    470: "Frosinone",
+    471: "Sassuolo",
+    586: "Torino",
+    5890: "US Lecce",
+    5911: "AC Monza"
+}
+
+const premierLeagueTeams: {[key: number]: string} = {
+    57: 'Arsenal',
+    58: 'Aston Villa',
+    61: 'Chelsea',
+    62: 'Everton',
+    63: 'Fulham',
+    64: 'Liverpool',
+    65: 'Man City',
+    66: 'Man United',
+    67: 'Newcastle',
+    68: 'Norwhich City',
+    73: 'Tottenham',
+    76: 'Wolverhampton',
+    328: 'Burnley',
+    338: 'Leicester City',
+    340: 'Southhampton',
+    341: 'Leeds United',
+    346: 'Watford',
+    351: 'Nottingham',
+    354: 'Crystal Palace',
+    356: 'Sheffield',
+    389: 'Luton Town',
+    397: 'Brighton Hove',
+    402: 'Brentford',
+    563: 'West Ham',
+    1044: 'Bournemouth'
 }
 
 export const competitions: {[key: number]: Competition} = {
@@ -57,123 +183,33 @@ export const competitions: {[key: number]: Competition} = {
     2001: {
         competition: CompetitionEnum.CHAMPIONS_LEAGUE,
         teams: {
-            65: 'Man City',
-            108: "Inter Milan"
+            ...bundesLigaTeams,
+            ...eredivisieTeams,
+            ...serieATeams,
+            ...laLigaTeams,
+            ...premierLeagueTeams
         }
     },
-    // 2002: {name: "Bundesliga"},
+    2002: {
+        competition: CompetitionEnum.BUNDESLIGA,
+        teams: bundesLigaTeams
+    },
     2003: {
         competition: CompetitionEnum.EREDIVISIE,
-        teams: {
-            666: 'FC Twente',
-            668: 'VVV Venlo',
-            671: 'Heracles',
-            672: 'Willem II',
-            673: 'SC Heerenveen',
-            674: 'PSV',
-            675: 'Feyenoord',
-            676: 'FC Utrecht',
-            677: 'FC Groningen',
-            678: 'Ajax',
-            679: 'Vitesse',
-            680: 'ADO Den Haag',
-            682: 'AZ',
-            683: 'RKC Waalwijk',
-            684: 'PEC Zwolle',
-            670: 'Excelsior',
-            718: 'Go Ahead Eagles',
-            1909: 'SC Cambuur',
-            1911: 'Almere City',
-            1914: 'FC Emmen',
-            1915: 'NEC Nijmegen',
-            1919: 'FC Volendam',
-            1920: 'Fortuna Sittard',
-            6806: 'Sparta'
-        }
+        teams: eredivisieTeams
     },
     2014: {
         competition: CompetitionEnum.LA_LIGA,
-        teams: {
-            77: "Atletic Club",
-            78: "Atlético",
-            79: "CA Osasuna",
-            80: "Espanyol",
-            81: "FC Barcalona",
-            82: "Getafe CF",
-            83: "Granada",
-            86: "Real Madrid",
-            87: "Rayo Vallecano",
-            89: "RCD Mallorca",
-            90: "Real Betis",
-            92: "Real Sociedad",
-            94: "Villareal CF",
-            95: "Valencia CF",
-            250: "Valladolid",
-            263: "Alavés",
-            264: "Cádiz CF",
-            267: "UD Almería",
-            275: "Las Palmas",
-            285: "Elche CF",
-            298: "Girona FC",
-            558: "RC Celta de Vigo",
-            559: "Sevilla FC"
-        }
+        teams: laLigaTeams
     },
     // 2015: {name: "Ligue 1"},
     2019: {
         competition: CompetitionEnum.SERIE_A,
-        teams: {
-            98: "AC Milan",
-            99: "Fiorentina",
-            100: "AS Roma",
-            102: "Atalanta",
-            103: "Bologna",
-            104: "Cagliari",
-            107: "Genoa",
-            108: "Inter Milan",
-            109: "Juventus",
-            110: "Lazio",
-            113: "Napoli",
-            115: "Udinese",
-            445: "Empoli",
-            450: "Verona",
-            455: "Salernitana",
-            470: "Frosinone",
-            471: "Sassuolo",
-            586: "Torino",
-            5890: "US Lecce",
-            5911: "AC Monza"
-        }
+        teams: serieATeams
     },
     2021: {
         competition: CompetitionEnum.PREMIER_LEAGUE,
-        teams: {
-            57: 'Arsenal',
-            58: 'Aston Villa',
-            61: 'Chelsea',
-            62: 'Everton',
-            63: 'Fulham',
-            64: 'Liverpool',
-            65: 'Man City',
-            66: 'Man United',
-            67: 'Newcastle',
-            68: 'Norwhich City',
-            73: 'Tottenham',
-            76: 'Wolverhampton',
-            328: 'Burnley',
-            338: 'Leicester City',
-            340: 'Southhampton',
-            341: 'Leeds United',
-            346: 'Watford',
-            351: 'Nottingham',
-            354: 'Crystal Palace',
-            356: 'Sheffield',
-            389: 'Luton Town',
-            397: 'Brighton Hove',
-            402: 'Brentford',
-            563: 'West Ham',
-            1044: 'Bournemouth'
-        }
+        teams: premierLeagueTeams
     }
 }
 
