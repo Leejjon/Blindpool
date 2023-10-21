bf41b11fb1d2204418e2cad822d8759188aec2a4
Leejjon <leejjon@gmail.com>
2023-10-21 10:27:29 +0200

    Updated locales.

--- blindpool-frontend/src/locales.ts
+++ blindpool-frontend/src/locales.ts
@@ -17,7 +17,7 @@ export const englishPageTitlesAndDescriptions: routeToPageAndDescriptionMapping 
 
 export const dutchPageTitlesAndDescriptions: routeToPageAndDescriptionMapping = {
     '/': {title: 'Blindepool', description: 'Een blindepool is een gokspel waar elke deelnemer een willekeurige voetbal uitslag krijgt. De deelnemer met de uitslag van de wedstrijd, wint.'},
-    '/about': {title: 'Blindepool - Over Blindepool', description: 'My name is Leon Liefting and I\'m a full stack developer.'},
+    '/about': {title: 'Blindepool - Over Blindepool', description: 'Mijn naam is Leon Liefting en ik ben een full stack developer (freelance) en ik ben graag betrokken in alle fases van software ontwikkeling.'},
     '/create': {title: 'Blindepool - Maak een pool', description: 'Voetbal kijken met vrienden? Maak een blindepool in 30 seconden. Gratis en geen account nodig.'},
     '/howto': {title: 'Blindepool - Hoe werkt het?', description: 'Stap voor stap uitgelegd hoe je een blindepool maakt en deelt met je vrienden.'}
 };
