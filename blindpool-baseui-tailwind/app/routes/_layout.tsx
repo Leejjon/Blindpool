d75b12c812e63a110654d29673b33bb04b9dc911
Leejjon <leejjon@gmail.com>
2024-06-16 01:46:52 +0200

    Fixed TypeScript issue in the link.

--- blindpool-baseui-tailwind/app/routes/_layout.tsx
+++ blindpool-baseui-tailwind/app/routes/_layout.tsx
@@ -1,10 +1,12 @@
 import { LinksFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 
+// Fixed like this: https://github.com/remix-run/remix/issues/4728
+// We use archivo regular: https://fonts.google.com/specimen/Archivo
 export const links: LinksFunction = () => {
     return [
         {rel: "preconnect", href: "https://fonts.googleapis.com"},
-        {rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: true},
+        {rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous"},
         {href: "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap", rel: "stylesheet"}
     ];
 }
