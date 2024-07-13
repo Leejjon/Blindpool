e48d3090a08bd6f1a5961fbb24f564e97c97c237
Leejjon <leejjon@gmail.com>
2024-07-13 21:40:59 +0200

    Used createRoutesFromElements to make the routes more concise.

--- blindpool-client/src/main.tsx
+++ blindpool-client/src/main.tsx
@@ -6,7 +6,7 @@ import theme from "./theme/theme";
 import './locales/i18n';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@mui/material";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Home from './views/home/Home';
 import About from './views/about/About';
 import CreatePool from './views/createpool/CreatePool';
@@ -19,34 +19,17 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-                path: "/about",
-                element: <About />
-            },
-            {
-                path: "/create",
-                element: <CreatePool />
-            },
-            {
-                path: "/howto",
-                element: <HowTo />
-            },
-            {
-                path: "/pool/:key",
-                element: <ViewPool />
-            }
-        ]
-    }
-]);
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index={true} element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/create" element={<CreatePool />} />
+            <Route path="/howto" element={<HowTo />} />
+            <Route path="/pool/:key" element={<ViewPool />} />
+        </Route>
+    )
+);
 
 root.render(
     <React.StrictMode>
