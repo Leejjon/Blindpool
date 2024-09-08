531f02343ceae74c9ea7273d40e8002e3f3fcbb3
Leejjon <leejjon@gmail.com>
2024-09-08 21:33:08 +0200

    Worked around the problem that caused the COULD NOT USE WINDOW.LOCATION.HOSTNAME error. If I wrap it in an exported function it's fine.

--- blindpool-client2/app/entry.client.tsx
+++ blindpool-client2/app/entry.client.tsx
@@ -4,11 +4,24 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 import theme from "./theme/theme";
-import './locales/i18n';
+import { initReactI18next } from "react-i18next";
 import { ThemeProvider } from "@mui/material";
 import { RemixBrowser } from "@remix-run/react";
+import { use } from "i18next";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import { getLocale, resources } from "./locales/translations";
+
+use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: getLocale(),
+    returnNull: false,
+    keySeparator: false, // we do not use keys in form messages.welcome
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
 
 startTransition(() => {
   hydrateRoot(
--- blindpool-client2/app/routes/_index.tsx
+++ blindpool-client2/app/routes/_index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
 import type { MetaFunction } from "@remix-run/node";
 import { useTranslation } from "react-i18next";
+import { getLocale, resources } from "../locales/translations";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
+    // { title: "Test" },
+    { title: `${resources[getLocale()].translation.TITLE} - ${resources[getLocale()].translation.BLINDPOOL_DEFINITION_TITLE}` },
     { name: "description", content: "Welcome to Remix (SPA Mode)!" },
   ];
 };
