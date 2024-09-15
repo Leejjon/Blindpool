0deacfd62dbad985c46d0ffda611ea2dbc2abd19
Leejjon <leejjon@gmail.com>
2024-09-15 23:04:11 +0200

    Fix the tests.

--- blindpool-client2/app/locales/i18n.test.ts
+++ blindpool-client2/app/locales/i18n.test.ts
@@ -1,4 +1,6 @@
-import {resources} from "./i18n";
+import { resources } from "./translations";
+import { describe, expect, test } from "vitest";
+
 
 describe('Resource bundle tests', () => {
     test('Verify if all keys are there in both the language bundles', () => {
