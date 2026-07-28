5ed0ccf442a654044ff62f6a743a7a25452a2d0f
Leejjon <leejjon@gmail.com>
2026-07-28 07:57:50 +0200

    Moving away from file based routing to defining everything once again.

--- blindpool-client/app/routes.ts
+++ blindpool-client/app/routes.ts
@@ -1,4 +1,11 @@
-import { flatRoutes } from "@react-router/fs-routes";
-import type {RouteConfig} from "@react-router/dev/routes";
+import {type RouteConfig, index, route, layout} from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig;
+export default [
+    layout("./routes/_layout.tsx", [
+        index("./routes/_layout._index.tsx"),
+        route("/about", "./routes/_layout.about.tsx"),
+        route("/create", "./routes/_layout.create.tsx"),
+        route("/howto", "./routes/_layout.howto.tsx"),
+        route("/pool/:key", "./routes/_layout.pool.$key.tsx"),
+    ])
+] satisfies RouteConfig;
