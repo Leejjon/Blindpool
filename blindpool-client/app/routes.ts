96a0e5963c53cbba3d1d087f3db5b789d43c0f6c
Leejjon <leejjon@gmail.com>
2026-07-29 08:50:52 +0200

    Getting rid of the file based routing

--- blindpool-client/app/routes.ts
+++ blindpool-client/app/routes.ts
@@ -1,11 +1,11 @@
 import {type RouteConfig, index, route, layout} from "@react-router/dev/routes";
 
 export default [
-    layout("./routes/_layout.tsx", [
-        index("./routes/_layout._index.tsx"),
-        route("/about", "./routes/_layout.about.tsx"),
-        route("/create", "./routes/_layout.create.tsx"),
-        route("/howto", "./routes/_layout.howto.tsx"),
-        route("/pool/:key", "./routes/_layout.pool.$key.tsx"),
+    layout("./routes/layout.tsx", [
+        index("./routes/index.tsx"),
+        route("/about", "./routes/about.tsx"),
+        route("/create", "./routes/create.tsx"),
+        route("/howto", "./routes/howto.tsx"),
+        route("/pool/:key", "./routes/pool.tsx"),
     ])
 ] satisfies RouteConfig;
